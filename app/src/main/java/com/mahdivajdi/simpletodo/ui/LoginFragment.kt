b3b8cec35b405e37eb616e22718aa18e3f789767
Mahdi Vajdi <mahdivajdii@gmail.com>
2022-12-10 19:02:26 +0330

    Added binding to LoginFragment.kt

--- app/src/main/java/com/mahdivajdi/simpletodo/ui/LoginFragment.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/ui/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.mahdivajdi.simpletodo.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.mahdivajdi.simpletodo.R
+import com.mahdivajdi.simpletodo.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
+    private var _binding: FragmentLoginBinding? = null
+    val binding: FragmentLoginBinding get() = _binding!!
 
     private val viewModel: LoginViewModel by viewModels {
         LoginViewModelFactory()
@@ -22,8 +21,14 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
