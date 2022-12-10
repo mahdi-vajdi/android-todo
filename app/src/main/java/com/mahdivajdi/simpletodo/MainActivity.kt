5e1705dbc1656d979450eb78ef7002c6666424d6
Mahdi Vajdi <mahdivajdii@gmail.com>
2022-12-10 11:48:09 +0330

    Initialize Binding and Navigation

--- app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mahdivajdi.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.mahdivajdi.simpletodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
