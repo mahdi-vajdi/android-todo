f001d946878aec8efb79224c269a35e93d2b9532
Mahdi Vajdi <mahdivajdii@gmail.com>
2022-12-25 18:39:37 +0330

    Taking app offline

--- app/src/main/java/com/mahdivajdi/simpletodo/ui/MainFragment.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/ui/MainFragment.kt
@@ -5,29 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.asLiveData
-import com.mahdivajdi.simpletodo.data.AuthRepository
-import com.mahdivajdi.simpletodo.data.local.UserPreferences
-import com.mahdivajdi.simpletodo.data.remote.AuthDataSource
-import com.mahdivajdi.simpletodo.data.remote.AuthServiceBuilder
 import com.mahdivajdi.simpletodo.databinding.FragmentMainBinding
-import com.mahdivajdi.simpletodo.ui.auth.AuthViewModel
-import com.mahdivajdi.simpletodo.ui.auth.AuthViewModelFactory
+
 
 class MainFragment : Fragment() {
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory()
-    }
-    private val authViewModel: AuthViewModel by activityViewModels {
-        AuthViewModelFactory(
-            AuthRepository(
-                AuthDataSource(AuthServiceBuilder.retrofitService),
-                UserPreferences(requireContext())
-            )
-        )
     }
 
     private var _binding: FragmentMainBinding? = null
@@ -44,10 +29,6 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val userPreferences = UserPreferences(requireContext())
-        userPreferences.refreshToken.asLiveData().observe(viewLifecycleOwner) {
-            binding.edittextUsername.text = it ?: "Token is null"
-        }
     }
 
     override fun onDestroy() {
