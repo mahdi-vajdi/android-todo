f8aa02e3c213fe153163ae74e1cc9c99d3c4ec26
Mahdi Vajdi <mahdivajdii@gmail.com>
2022-12-26 10:39:26 +0330

    Added recyclerview for tasks

--- app/src/main/java/com/mahdivajdi/simpletodo/ui/MainFragment.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/ui/MainFragment.kt
@@ -6,18 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.mahdivajdi.simpletodo.data.TaskRepository
+import com.mahdivajdi.simpletodo.data.local.AppDatabase
 import com.mahdivajdi.simpletodo.databinding.FragmentMainBinding
+import com.mahdivajdi.simpletodo.ui.adapter.TaskListAdapter
 
 
 class MainFragment : Fragment() {
 
-    private val mainViewModel: MainViewModel by viewModels {
-        MainViewModelFactory()
+    private val taskViewModel: TaskViewModel by viewModels {
+       TaskViewModelFactory(
+           TaskRepository(
+               AppDatabase.getDatabase(requireContext()).taskDao()
+           )
+       )
     }
 
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding get() = _binding!!
 
+    private lateinit var taskListAdapter: TaskListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +38,17 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Initiate recyclerview
+        taskListAdapter = TaskListAdapter()
+        binding.RecyclerViewMain.adapter = taskListAdapter
+        taskViewModel.getTasks().observe(viewLifecycleOwner) {
+            taskListAdapter.submitList(it)
+        }
+
+        binding.fabMainAdd.setOnClickListener {
+            // TODO: implement adding task dialog
+        }
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/mahdivajdi/simpletodo/ui/adapter/TaskListAdapter.kt
@@ -0,0 +1,57 @@
+package com.mahdivajdi.simpletodo.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mahdivajdi.simpletodo.databinding.ListItemTaskBinding
+import com.mahdivajdi.simpletodo.domain.model.TaskDomainModel
+
+class TaskListAdapter : ListAdapter<TaskDomainModel, TaskListAdapter.TaskViewHolder>(DiffCallback) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(
+            ListItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class TaskViewHolder(private val binding: ListItemTaskBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(task: TaskDomainModel) {
+            binding.apply {
+                textViewTasksId.text = task.id.toString()
+                textViewTasksTitle.text = task.title
+                textViewTasksTimestamp.text = task.timestamp.toString()
+                textViewTasksDescription.text = task.description
+                textViewTasksDone.text = task.done.toString()
+                textViewTasksTags.text = task.tags.toString()
+            }
+        }
+
+    }
+
+    companion object {
+        private val DiffCallback = object : DiffUtil.ItemCallback<TaskDomainModel>() {
+            override fun areItemsTheSame(
+                oldItem: TaskDomainModel,
+                newItem: TaskDomainModel,
+            ): Boolean {
+                return (oldItem.id == newItem.id)
+            }
+
+            override fun areContentsTheSame(
+                oldItem: TaskDomainModel,
+                newItem: TaskDomainModel,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
