399f4f8ea088c8ac9ae226e46e2b5c0f8b625881
Mahdi Vajdi <mahdivajdii@gmail.com>
2022-12-30 18:39:28 +0330

    feat: Added bottom navigation bar.

--- app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mahdivajdi.simpletodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +22,10 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.categoriesFragment, R.id.tasksFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
