765651b5a09f2aed76382dc6a23a28bc4c70f80b
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-01-01 19:09:44 +0330

    fix: Fixed absence of new task's category. now user can select category and task object can be inserted into database.

--- app/src/main/java/com/mahdivajdi/simpletodo/ui/task/AddTaskFragment.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/ui/task/AddTaskFragment.kt
@@ -4,32 +4,65 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LiveData
+import com.mahdivajdi.simpletodo.App
+import com.mahdivajdi.simpletodo.R
+import com.mahdivajdi.simpletodo.data.repository.CategoryRepository
+import com.mahdivajdi.simpletodo.data.repository.TaskRepository
 import com.mahdivajdi.simpletodo.databinding.AddTaskDialogBinding
+import com.mahdivajdi.simpletodo.domain.model.Category
 import com.mahdivajdi.simpletodo.domain.model.Task
 import java.time.Instant
 
 class AddTaskFragment(private val newTask: (task: Task) -> Unit) : DialogFragment() {
 
+    private val taskViewModel: TaskViewModel by activityViewModels {
+        TaskViewModelFactory(TaskRepository((activity?.application as App).database.taskDao()),
+            CategoryRepository((activity?.application as App).database.categoryDao()))
+    }
+
     private var _binding: AddTaskDialogBinding? = null
     private val binding: AddTaskDialogBinding get() = _binding!!
 
+    private lateinit var spinnerAdapter: ArrayAdapter<Category>
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return activity?.let {
-            _binding = AddTaskDialogBinding.inflate(layoutInflater)
-            val builder = AlertDialog.Builder(it)
+        _binding = AddTaskDialogBinding.inflate(layoutInflater)
+
+        // Initiate spinner
+        spinnerAdapter =
+            ArrayAdapter<Category>(requireContext(), android.R.layout.simple_spinner_item)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerAddTaskCategory.adapter = spinnerAdapter
+
+        // Initiate list for category names and ids
+        taskViewModel.getCategories().observe(this) { categories ->
+            spinnerAdapter.addAll(categories)
+            spinnerAdapter.notifyDataSetChanged()
+        }
+
+        return activity?.let { fragmentActivity ->
+            val builder = AlertDialog.Builder(fragmentActivity)
             builder.setView(binding.root)
                 .setPositiveButton("Save", DialogInterface.OnClickListener { _, _ ->
                     val instant = Instant.now()
-                    // TODO: Fix the absence of category id in the task model
+                    val spinner = binding.spinnerAddTaskCategory
                     val task = Task(
                         title = binding.editTextAddTaskTitle.text.toString(),
                         description = binding.editTextAddTaskDescription.text.toString(),
                         timestamp = instant.epochSecond,
+                        taskCategoryId = (spinner.getItemAtPosition(spinner.selectedItemPosition) as Category).categoryId
                     )
                     newTask(task)
                     dialog?.cancel()
-                }).setNegativeButton("Cancel" ,DialogInterface.OnClickListener { _, _ ->
+                }).setNegativeButton("Cancel", DialogInterface.OnClickListener { _, _ ->
                     dialog?.cancel()
                 })
             builder.create()
