04d28d95e02a56e74252f369538a903e54e25074
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-01-05 19:24:36 +0330

    feature: Added add task functionality to bottom nav fab

--- app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
+++ app/src/main/java/com/mahdivajdi/simpletodo/MainActivity.kt
@@ -2,14 +2,29 @@ package com.mahdivajdi.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.mahdivajdi.simpletodo.data.repository.CategoryRepository
+import com.mahdivajdi.simpletodo.data.repository.TaskRepository
 import com.mahdivajdi.simpletodo.databinding.ActivityMainBinding
+import com.mahdivajdi.simpletodo.ui.task.AddTaskFragment
+import com.mahdivajdi.simpletodo.ui.task.TaskViewModel
+import com.mahdivajdi.simpletodo.ui.task.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskViewModelFactory(
+            TaskRepository((this.application as App).database.taskDao()),
+            CategoryRepository((this.application as App).database.categoryDao())
+        )
+    }
 
     private lateinit var navController: NavController
 
@@ -32,6 +47,15 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationBar.background = null
         binding.bottomNavigationBar.menu.getItem(2).isEnabled = false
         binding.bottomNavigationBar.setupWithNavController(navController)
+
+        // Add new task using fab button
+        binding.fabMainAddTask.setOnClickListener {
+            val addTaskFragment = AddTaskFragment {
+                Log.i("task", "onViewCreated: newTask= $it")
+                taskViewModel.insertTask(it)
+            }
+            addTaskFragment.show(supportFragmentManager, "add_task")
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
